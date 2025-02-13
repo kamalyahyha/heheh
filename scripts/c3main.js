3a82106d131e57b2f34eaf86298ee666547a0947
kamalyahyha <kamalyahya284@gmail.com>
2025-02-13 15:31:52 +0300

    awd

--- scripts/c3main.js
+++ scripts/c3main.js
@@ -1,4 +1,4 @@
-import "./../box2d.wasm.js";
-import "./c3runtime.js";
-import "./objRefTable.js";
-import "./project/instant-games-bridge.js";
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/box2d.wasm.js";
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/c3runtime.js";
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/objRefTable.js";
+import "https://cdn.jsdelivr.net/gh/kamalyahyha/heheh@main/project/instant-games-bridge.js";
